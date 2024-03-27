334b46efcf4ceb0ca98c0d2832fc5cb8bc43e9d4
DavidLee36 <104027396+DavidLee36@users.noreply.github.com>
2024-03-26 18:32:09 -0700

    Work to make back button work to close modal

--- src/pages/components/Modal.jsx
+++ src/pages/components/Modal.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Rating from './Rating.jsx';
 import Providers from './Providers.jsx';
@@ -9,43 +9,53 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar as solidStar } from '@fortawesome/free-solid-svg-icons';
 import { faStar as regularStar } from '@fortawesome/free-regular-svg-icons';
 
-
 const Modal = ({ movie, onClose }) => {
     const [favorited, setFavorited] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
         setFavorited(movieIsFavorited(movie.id));
     }, [movie]);
 
     const handleEscape = (e) => {
-        if(e.key === 'Escape') {
-
-            //If on favorites page, refresh
-            if(window.location.pathname.includes('/favorites')) {
-                navigate('/favorites');
-            }
-            onClose();
+        if (e.key === 'Escape') {
+            closeAndNavigate();
         }
+    }
+
+    const closeAndNavigate = () => {
+        // If on favorites page, refresh
+        if (window.location.pathname.includes('/favorites')) {
+            navigate('/favorites');
+        }
+        onClose();
     }
 
     useEffect(() => {
         window.addEventListener('keydown', handleEscape);
+        window.history.pushState(null, null, window.location.href); // Push new state into history when modal opens
+        const handlePopState = () => {
+            closeAndNavigate();
+        };
+
+        window.addEventListener('popstate', handlePopState);
+
         return () => {
             window.removeEventListener('keydown', handleEscape);
+            window.removeEventListener('popstate', handlePopState);
+            window.history.go(1); // Move forward in history stack if modal is closed to prevent back action from reopening it
         };
-    }, [])
-
-    const navigate = useNavigate();
+    }, []);
 
     const handleMoreInfo = () => {
         navigate(`/additional-info#${movie.id}`, { state: { movie } });
     }
 
     const changeFavStatus = () => {
-        if(favorited) {
+        if (favorited) {
             removeFromFavorites(movie.id);
             setFavorited(false);
-        }else {
+        } else {
             addToFavorites(movie);
             setFavorited(true);
         }
@@ -54,7 +64,7 @@ const Modal = ({ movie, onClose }) => {
     return (
         <div className='modal-overlay' onClick={(e) => e.target.classList.contains('modal-overlay') && onClose()}>
             <div className="modal-container">
-                <div className="image-container">
+            <div className="image-container">
                     {movie.backdrop_path !== null ?
                         <img src={`${BASE_IMG_URL}${movie.backdrop_path}`} alt="image not found" className='modal-backdrop' /> :
                         <img src={`${BASE_IMG_URL}${movie.poster_path}`} alt="image not found" className='modal-poster' />
@@ -89,4 +99,4 @@ const Modal = ({ movie, onClose }) => {
     )
 }
 
-export default Modal
+export default Modal;
